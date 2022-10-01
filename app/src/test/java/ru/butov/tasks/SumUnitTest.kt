1dcd201df261d61c30bca7fb3fbf4d0904827ec7
Anton Butov <butov6101@gmail.com>
2022-10-01 19:37:29 +0300

    add sum task

---
+++ app/src/test/java/ru/butov/tasks/SumUnitTest.kt
@@ -0,0 +1,16 @@
+package ru.butov.tasks
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class SumUnitTest {
+
+
+    @Test
+    fun case() {
+        val sum = Sum()
+        val expected = arrayListOf(0, 4, 1, 3)
+        val test = sum.twoSum(listOf(2, 1, 11, 8, 7), target = 9)
+        assertEquals(test, expected)
+    }
+}
