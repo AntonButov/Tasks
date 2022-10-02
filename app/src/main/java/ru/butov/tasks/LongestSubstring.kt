ff363e9bb9ef3d1bae95609110f64c91e2e6a890
Anton Butov <butov6101@gmail.com>
2022-10-02 13:47:07 +0300

    add longest substring

---
+++ app/src/main/java/ru/butov/tasks/LongestSubstring.kt
@@ -0,0 +1,31 @@
+package ru.butov.tasks
+
+class LongestSubstring {
+
+    fun getCount(source: String): Int =
+        source.mapIndexed { index, _ ->
+            val substring = source.substring(index until source.count())
+            substring.fold("") { acc, char ->
+                acc.addIfNot(char)
+            }
+                .replace(";", "")
+                .count()
+        }
+            .max()
+
+    private fun String.addIfNot(char: Char): String {
+        if (isClose()) return this
+        if (contains(char))
+            return add(';')
+        return add(char)
+    }
+
+    private fun String.add(char: Char): String =
+        buildString {
+            append(this@add)
+            append(char)
+        }
+
+    private fun String.isClose(): Boolean =
+        contains(';')
+}
