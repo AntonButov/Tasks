b9bc17d16baeacd5437267e7a8df159a14da0c3e
Anton Butov <butov6101@gmail.com>
2022-10-06 08:15:33 +0300

    add second longest substring

---
+++ app/src/main/java/ru/butov/tasks/LongestSubstringSecond.kt
@@ -0,0 +1,26 @@
+package ru.butov.tasks
+
+class LongestSubstringSecond {
+
+    fun getCount(source: String): Int =
+        source.mapIndexed { index, _ ->
+            source.substring(index until source.count())
+        }
+            .map { it.toLists() }
+            .flatten()
+            .filter { isIndividual(it) }
+            .maxBy { it.length }
+            .count()
+
+    private fun isIndividual(string: String) =
+        string.none { it in string.withOut(it) }
+
+    private fun String.withOut(ch: Char): String =
+        replaceFirst(ch.toString(), "")
+
+
+    private fun String.toLists(): List<String> =
+        mapIndexed { index, _ ->
+            substring(0..index)
+        }
+}
