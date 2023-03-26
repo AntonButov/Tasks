2a72f0ba5df008b913748c5acd05fe7517eaebfc
Anton Butov <butov6101@gmail.com>
2023-03-26 11:18:42 +0300

    add Max Increase to Keep City Skyline

--- app/src/main/java/ru/butov/tasks/MaxIncreaseToKeepCitySkyline.kt
+++ app/src/main/java/ru/butov/tasks/MaxIncreaseToKeepCitySkyline.kt
@@ -3,5 +3,15 @@ package ru.butov.tasks
 class MaxIncreaseToKeepCitySkyline {
 
     fun increase(inputList: List<List<Int>>): List<List<Int>> =
-        listOf()
+        inputList.mapIndexed { y, list ->
+            list.mapIndexed { x, _ ->
+                Math.min(inputList.getMaxVertical(x), inputList.getMaxHorizontal(y))
+            }
+        }
+
+    private fun List<List<Int>>.getMaxHorizontal(y: Int): Int =
+        this[y].max()
+
+    private fun List<List<Int>>.getMaxVertical(x: Int): Int =
+        map { it[x] }.max()
 }
