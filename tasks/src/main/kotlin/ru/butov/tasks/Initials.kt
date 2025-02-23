f39c84c412697cfab4bb3f291cfe2fc902eaa560
Anton Butov <butov6101@gmail.com>
2025-02-23 12:40:01 +0300

    add initials

--- tasks/src/main/kotlin/ru/butov/tasks/Initials.kt
+++ tasks/src/main/kotlin/ru/butov/tasks/Initials.kt
@@ -1,10 +1,3 @@
 interface Initials {
     fun format(): String
 }
-
-class InitialsImpl(input: String): Initials {
-    override fun format(): String {
-        TODO("Not yet implemented")
-    }
-
-}
