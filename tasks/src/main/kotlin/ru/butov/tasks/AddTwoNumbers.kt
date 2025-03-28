d1aaecee84cd57114be1e15832d76cb1a6921bb5
Anton Butov <butov6101@gmail.com>
2025-03-28 09:44:22 +0300

    refactor

--- tasks/src/main/kotlin/ru/butov/tasks/AddTwoNumbers.kt
+++ tasks/src/main/kotlin/ru/butov/tasks/AddTwoNumbers.kt
@@ -9,18 +9,15 @@ class AddTwoNumbers {
     fun add(listFirst: List<Int>, listSecond: List<Int>): List<Int> {
         var index = 0
         val result = LinkedList<Int>()
-        var isOver = false
+        var over = 0
         while (index < max(listFirst.size, listSecond.size)) {
             val indexFirst = listFirst.size - 1 - index
             val indexSecond = listSecond.size - 1 - index
             val firstNum = if (indexFirst >= 0) listFirst[indexFirst] else 0
             val secondNum = if (indexSecond >= 0) listSecond[indexSecond] else 0
-            var resNum = firstNum + secondNum
-            if (isOver) resNum++
-            if (resNum >= 10) {
-                isOver = true
-                resNum %= 10
-            } else isOver = false
+            var resNum = firstNum + secondNum + over
+            over = resNum/10
+            resNum %= 10
             result.addLast(resNum)
             index ++
         }
