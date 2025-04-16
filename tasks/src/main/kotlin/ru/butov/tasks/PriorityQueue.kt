ecb2f30426b300af1b17488b26d405f2e52cb256
Anton Butov <butov6101@gmail.com>
2025-04-16 09:36:45 +0300

    priority queue

--- tasks/src/main/kotlin/ru/butov/tasks/PriorityQueue.kt
+++ tasks/src/main/kotlin/ru/butov/tasks/PriorityQueue.kt
@@ -6,21 +6,20 @@ interface PriorityQueue {
     fun getResult(input: List<Int>): List<Int>
 }
 
-class PriorityQueueImpl: PriorityQueue {
+class PriorityQueueImpl : PriorityQueue {
     private val priorityQueue = java.util.PriorityQueue(
         Comparator<Num> { o1, o2 ->
-                o2.counts - o1.counts
+            o2.counts - o1.counts
         }
     )
-    private val map = mutableMapOf<Int,Int>()
+    private val map = mutableMapOf<Int, Int>()
 
     override fun getResult(input: List<Int>): List<Int> {
         input.forEach { number ->
-            map.getOrDefault(number, 0).let {
-                priorityQueue.remove(Num(number, it))
-                map[number] = it + 1
-                priorityQueue.add(Num(number, it + 1))
-            }
+            map[number] = map.getOrDefault(number, 0) + 1
+        }
+        map.forEach { (k, c) ->
+            priorityQueue.add(Num(k, c))
         }
         return priorityQueue.filter { it.counts > 1 }.map { it.key }.toList()
     }
